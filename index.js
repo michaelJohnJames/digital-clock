09df5088dcd1b041be2d31f0bba4ef4dedf75f5c
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-10-13 13:22:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var box = document.getElementById('box')
+var clock = document.getElementById('clock-container')
+
+
+function logTime() {
+
+  var time = new Date();
+  var hrs = time.getHours() > 9 ? time.getHours() : '0' + time.getMinutes();
+  var min = time.getMinutes() > 9 ? time.getMinutes() : '0' + time.getMinutes();
+  var sec = time.getSeconds() > 9 ? time.getSeconds() : '0' + time.getSeconds();
+
+  clock.innerHTML = (hrs + ':' + min + ':' + sec);
+  //var stringclock = clock.innerHTML.toString();
+
+}
+
+
+
+
+
+setInterval(logTime, 1000);
+
+
+//clock.addEventListener('load', logTime);
