a04843fd496a3bc911fb39d8d15dd43cc5be8826
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-10-14 20:59:51 -0400

    rearrange box

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var box3 = document.getElementById('box3');
 var box2 = document.getElementById('box2');
 var clock = document.getElementById('clock');
-var hideBox = function() { box3.classList.add("hidden") };
-var showBox = function() { box3.classList.remove("hidden") };
+var hideBox = function(box3) { box3.classList.add("hidden") };
+var showBox = function(box3) { box3.classList.remove("hidden") };
 
 
 
@@ -21,25 +21,32 @@ function logTime() {
 
 }
 
-var time = new Date();
-var hrs = time.getHours() > 9 ? time.getHours() : '0' + time.getMinutes();
-var min = time.getMinutes() > 9 ? time.getMinutes() : '0' + time.getMinutes();
-var sec = time.getSeconds() > 9 ? time.getSeconds() : '0' + time.getSeconds();
+
 
 function redBox() {
-  if ( sec != 00 ) {
-  box3.classList.add("hidden");
-}  else {  box3.classList.remove("hidden"); }
-}
+  var time = new Date();
+  var seconds = time.getSeconds();
 
-function flashRed() {
-if ( sec >= 55 ) {
-  box2.style.backgroundColor = red;
-}
+ if ( seconds > 10 ) {
+  document.getElementById("box3").classList.add('hidden');
+}  else {   document.getElementById("box3").classList.remove('hidden'); }
 }
 
 
+function redFlash() {
+  var time = new Date();
+  var seconds = time.getSeconds();
 
+  if ( seconds > "49" ) {
+  document.getElementById("clock").style.color = "red";
+  document.getElementById("headline").style.color = "red";
+} else { document.getElementById("clock").style.color = "black";
+  document.getElementById("headline").style.color = "white";}
+}
+
+
+
+
+setInterval(redFlash, 1000);
 setInterval(logTime, 1000);
 setInterval(redBox, 1000);
-setInterval(flashRed, 1000);
